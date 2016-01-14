1a935f4ad37c14f188e132f730a3401c8ddefb3e
hudbuz <hzb2@cornell.edu>
2016-01-14 19:19:48 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+
+
+  def job=(job_string)
+    @job = job_string
+  end
+
+  def job
+    @job
+  end
+
+  def name=(name_string)
+    @name = name_string
+  end
+
+  def name
+    @name
+  end
+end
